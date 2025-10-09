3e907e6b075e841723ff4acc5898b23298006b6d
unknown <valenciano3192006@gmail.com>
2025-10-09 10:14:45 +0200

    09.10.2025

---
+++ src/datastructure/SortingTest.java
@@ -0,0 +1,22 @@
+package datastructure;
+
+public class SortingTest {
+	
+	public static void main(String[] args) {
+		
+		String[] names = {"Pablo", "Juan", "Ana", "Justo", "Ruben"};
+		int[] scores = {12, 1000, 3423, 34, 2, 20, 3, 2};
+		
+		sort(scores);
+		
+	}
+
+	private static void sort(int[] arr) {
+		
+		for (int i = 0; i < arr.length; i++) {
+			int j = arr[i];
+		}
+		
+	}
+
+}
