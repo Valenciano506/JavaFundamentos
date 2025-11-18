e51585a28bd3161ec374c305a52e91a691145f58
Valenciano506 <valenciano3192006@gmail.com>
2025-11-18 13:18:50 +0100

    Implementación del algoritmo 'add' en un array flexible

--- src/datastructure/FlexibleArrayTest.java
+++ src/datastructure/FlexibleArrayTest.java
@@ -4,9 +4,14 @@ public class FlexibleArrayTest {
 
     public static void main(String[] args) {
 
-        FlexibleArray<Integer> flexibleArray = new FlexibleArray<>();
+        FlexibleArray<Integer> flexibleArray = new FlexibleArray<>(3);
         System.out.println("The current size = " + flexibleArray.size());
-        flexibleArray.add(3);
+      
+        for (int i = 0; i < 5; i++) {
+        	  flexibleArray.add(3 + i);
+		}
+        
+        System.out.println("The current size = " + flexibleArray.size());
         flexibleArray.remove(4);
 
     }
