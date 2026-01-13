bafddde11da42a49c4d97327f4874b7690ce1a29
unknown <valenciano3192006@gmail.com>
2026-01-13 13:09:58 +0100

    student grading system

---
+++ src/oopmodeling/gradingsystem/Student.java
@@ -0,0 +1,21 @@
+package oopmodeling.gradingsystem;
+
+import java.util.ArrayList;
+
+public class Student {
+	
+	//attributes
+	private ArrayList<Double> marks;
+	
+	//constructors
+	
+	
+	
+	//Behaviors:
+	//1. Calculate the average note
+	
+	//2. Find the highest note
+	
+	//3. Find the lowest note
+
+}
---
+++ src/oopmodeling/gradingsystem/StudentGradingApp.java
@@ -0,0 +1,15 @@
+package oopmodeling.gradingsystem;
+
+import java.util.Scanner;
+
+public class StudentGradingApp {
+	
+	private static Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		
+		
+
+	}
+
+}
