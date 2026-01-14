f40f4bda539244a267b8efa803ba30396281f8b0
unknown <valenciano3192006@gmail.com>
2026-01-14 14:39:40 +0100

    data input stream

--- src/javaio/BinaryDataOperations.java
+++ src/javaio/BinaryDataOperations.java
@@ -1,12 +1,14 @@
 
 package javaio;
 
+import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
 /*
- * 
+ * Write and read binary data to and from a file
  *
  * @author Valenciano
  * 14 ene 2026
@@ -15,14 +17,37 @@ import java.io.IOException;
 public class BinaryDataOperations {
 
 	public static void main(String[] args) {
+		
+		writeBinaryData();
+		readBinaryData();
+		
+	}
+
+	private static void writeBinaryData() {
+		
 		//A variable with null initialization
-		//DataOutputStream os = null;		
-		try {
-			DataOutputStream os = new DataOutputStream(new FileOutputStream(""));
-			os.writeChar(34);
+				//DataOutputStream os = null;		
+				try(DataOutputStream os = new DataOutputStream(new FileOutputStream("data.bat"))) {
+					os.writeChar(25);
+					os.writeInt(32);
+					os.writeUTF("Pablo is Maricon");
+					//os.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+				
+	}
+
+	private static void readBinaryData() {
+	
+		try (DataInputStream is = new DataInputStream(new FileInputStream(""))){
+			is.readChar();
+			is.readInt();
+			is.readUTF();
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
+		
 	}
 
 }
