a8def74fbf39608abeac1e47ffafa0daf37e41ac
unknown <valenciano3192006@gmail.com>
2026-02-13 11:38:38 +0100

    list error debug

--- src/datastructure/list/List.java
+++ src/datastructure/list/List.java
@@ -12,9 +12,42 @@ public class List<E> {
 	//The reference to the first element of the list 
 	private Node<E> first;
 	
-	public void add(Node<E> node) {
+	public void add(Node<E> node) throws Exception {
+		//1. Check the parameter if it is valid
+		if(node == null) {
+			throw new Exception("You cannot pass a null node to the list.");
+		}
 		
-		last.next = node;
+		//2. Add the node to the tail of the list
+		//2.1 Check first if the head element is null, in case of null, it means that the list is empty
+		if(first == null) {//If the list is empty
+			//The list is empty
+			last = node;
+			first = node;
+		}
+
+		else {
+			last.next = node;
+			last = node;
+		}		
+		
+	}
+
+	/**
+	 * @return the last
+	 */
+	public Node<E> getLast() {
+		return last;
+	}
+
+	/**
+	 * @return the first
+	 */
+	public Node<E> getFirst() {
+		return first;
+	}
+	
+	public void printList() {
 		
 	}
 
@@ -22,8 +55,11 @@ public class List<E> {
 
 class Node<E> {
 	
+	public Node(E e) {
+		data = e;
+	}
 	//The data stored in a node of the list
-	private E e;
+	private E data;
 	//Each node needs to pint to the next node, we use this variable to do it
 	protected Node<E> next;
 	
--- src/datastructure/list/ListTest.java
+++ src/datastructure/list/ListTest.java
@@ -7,7 +7,7 @@ package datastructure.list;
  */
 public class ListTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		
 		Object obj1 = new Object();
 		Object obj2 = new Object();
@@ -15,7 +15,15 @@ public class ListTest {
 		System.out.println(obj2);
 		
 		List<Integer> list = new List<Integer>();
-		list.add(new Node());
+		System.out.println("The first node of the list is: " + list.getFirst());
+		System.out.println("The last node of the list is: " + list.getLast());
+		//list.add(null);
+		list.add(new Node(30));
+		list.add(new Node(20));
+		list.add(new Node(10));
+		list.add(new Node(40));
+		list.add(new Node(50));
+		list.add(new Node(60));
 
 	}
 
