654560ace6936473f6f78cabcb0846a67d46d89b
unknown <valenciano3192006@gmail.com>
2026-04-28 12:58:49 +0200

    reflection task 1

--- src/reflection/ReflectionTest.java
+++ src/reflection/ReflectionTest.java
@@ -1,5 +1,9 @@
 package reflection;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import oopmodeling.adressbook.AdressBook;
 
 /*
@@ -13,6 +17,28 @@ public class ReflectionTest {
 		Class<AdressBook> cls = AdressBook.class;
 		Class<AdressBook> cls2 = (Class<AdressBook>) new AdressBook().getClass();
 		Class<?> cls3 = Class.forName("oopmodeling.adressbook.AdressBook");
+		
+		inspectClass(cls);
+	}
+
+	private static void inspectClass(Class<AdressBook> cls) {
+
+		cls.getDeclaredConstructors();
+		Method[] ms = cls.getDeclaredMethods();
+		for (int i = 0; i < ms.length; i++) {
+			System.out.println("Method name is " + ms[i].getName());
+			System.out.println("Method type is " + ms[i].getParameters());
+		}
+
+		for(Field field : cls.getDeclaredFields()) {
+			System.out.println("Field name is " + field.getName());
+			System.out.println("Field type is " + field.getType());
+		}
+
+		for(Annotation annotation : cls.getAnnotations()) {
+			System.out.println("Annoation type is " + annotation.annotationType());
+		}
+		
 	}
 
 }
